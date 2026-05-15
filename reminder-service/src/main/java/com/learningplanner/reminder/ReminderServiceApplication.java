364fd40f191a744ee629fbe486c6fcdd13952807
itcast <hello@itcast.cn>
2026-05-15 17:09:09 +0800

    fix: add @EnableFeignClients to ReminderServiceApplication

--- reminder-service/src/main/java/com/learningplanner/reminder/ReminderServiceApplication.java
+++ reminder-service/src/main/java/com/learningplanner/reminder/ReminderServiceApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.learningplanner.reminder", "com.learningplanner.common"})
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableFeignClients(basePackages = "com.learningplanner.reminder.feign")
 @MapperScan("com.learningplanner.reminder.repository")
 public class ReminderServiceApplication {
     public static void main(String[] args) {
